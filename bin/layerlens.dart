824a251ca182b0936f257d8e2ae00ac17c93e49f
Polina Cherkasova <polinach@google.com>
2022-11-27 23:34:44 -0800

    Update layerlens.dart

--- bin/layerlens.dart
+++ bin/layerlens.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 void main(List<String> args) {
   print('generating files');
-  File('LAYERS.MD').writeAsStringSync(DateTime.now().toString());
+  File('LAYERS.MD').writeAsStringSync(DateTime.now().toString() + '\n');
 }
